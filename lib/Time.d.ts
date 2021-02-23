45a653a9ddff44bff9851d8d9670e469fda9080a
Ntonas Efstathios <xboxlivegradmin@gmail.com>
2021-02-23 14:25:21 +0200

    chore: added lib folder on git

---
+++ lib/Time.d.ts
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import { Component } from 'react';
+import { ViewStyle, TextStyle } from 'react-native';
+import { LeftRightStyle, IMessage } from './Models';
+export interface TimeProps<TMessage extends IMessage> {
+    isMyMessage: boolean;
+    position: 'left' | 'right';
+    currentMessage?: TMessage;
+    containerStyle?: LeftRightStyle<ViewStyle>;
+    timeTextStyle?: LeftRightStyle<TextStyle>;
+    timeFormat?: string;
+    dateFormat?: string;
+    accessibilityHint?: string;
+}
+export default class Time<TMessage extends IMessage = IMessage> extends Component<TimeProps<TMessage>> {
+    static contextTypes: {
+        getLocale: PropTypes.Requireable<(...args: any[]) => any>;
+    };
+    static defaultProps: {
+        position: string;
+        currentMessage: {
+            createdAt: null;
+        };
+        containerStyle: {};
+        timeFormat: string;
+        timeTextStyle: {};
+    };
+    static propTypes: {
+        accessibilityHint: PropTypes.Requireable<string>;
+        position: PropTypes.Requireable<string>;
+        currentMessage: PropTypes.Requireable<object>;
+        containerStyle: PropTypes.Requireable<PropTypes.InferProps<{
+            left: PropTypes.Requireable<number | boolean | object>;
+            right: PropTypes.Requireable<number | boolean | object>;
+        }>>;
+        timeFormat: PropTypes.Requireable<string>;
+        timeTextStyle: PropTypes.Requireable<PropTypes.InferProps<{
+            left: PropTypes.Requireable<number | boolean | object>;
+            right: PropTypes.Requireable<number | boolean | object>;
+        }>>;
+    };
+    render(): JSX.Element | null;
+}
